e03e0f0cec95dba8868a19830e18cc69f447197c
elcbil <nabilahdaa@gmail.com>
2025-03-24 23:05:39 +0700

    add stock to database

--- function.php
+++ function.php
@@ -4,4 +4,20 @@ session_start();
 
 $conn = mysqli_connect("localhost", "root","", "stockbarang");
 
+
+//menambah barang baru
+
+if(isset($_POST['addnewbarang'])){
+    $namabarang = $_POST['namabarang'];
+    $deskripsi = $_POST['deskripsi'];
+    $stock = $_POST['stock'];
+
+    $addtotable = mysqli_query($conn, "insert into stock (namabarang, deskripsi, stock) values('$namabarang', '$deskripsi', '$stock')");
+    if($addtotable){
+        header('location: index.php');
+    } else {
+        echo 'Gagal';
+        header('location:inddex.php');
+    }
+}
 ?>
